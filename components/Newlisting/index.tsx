9d2962d268845ee13ee2087610015d1d582d782e
Lucas <loukaka41@gmail.com>
2022-03-30 22:36:29 +0200

    go

--- components/Newlisting/index.tsx
+++ components/Newlisting/index.tsx
@@ -6,10 +6,18 @@ export default function Newlisting({tokens}) {
       <>
       
       <tr>
-        <td className="token-id">{token.id}</td>
+        <td className="token-id"></td>
         <td className="token-name"><img src={token.logo} className="token-logo" alt="token logo" />{token.name}<span className="token-symbol">{token.symbol}</span></td>
-        <td className="token-links">{token.chat ? <a href={token.chat}><img className="links-icons" src="telegram.png"></img></a> : ""}{token.discord ? <a href={token.discord}><img className="links-icons" src="discord.png"></img></a> : ""}{token.website ? <a href={token.website}><img className="links-icons" src="website.png"></img></a> : ""}{token.twitter ? <a href={token.twitter}><img className="links-icons" src="twitter.png"></img></a> : ""}</td>
-        <td className="token-contract">{token.contract.substring(0, 4) + '..' + token.contract.substring(token.contract.length - 4, token.contract.length)}</td>
+        <td className="token-links">
+          {token.chat ? <a href={token.chat} target="_blank"><img className="links-icons" src="telegram.png"></img></a> : ""}
+          {token.discord ? <a href={token.discord} target="_blank"><img className="links-icons" src="discord.png"></img></a> : ""}
+          {token.website ? <a href={token.website} target="_blank"><img className="links-icons" src="website.png"></img></a> : ""}
+          {token.twitter ? <a href={token.twitter} target="_blank"><img className="links-icons" src="twitter.png"></img></a> : ""}</td>
+        <td className="token-contract">
+          {token.blockchain == "BNB" && <a className="tokenURL" href={"https://bscscan.com/token/" + token.contract} target="_blank">{token.contract.substring(0, 4) + '..' + token.contract.substring(token.contract.length - 4, token.contract.length)}</a>}
+          {token.blockchain == "Ethereum" && <a className="tokenURL" href={"https://etherscan.com/token/" + token.contract} target="_blank">{token.contract.substring(0, 4) + '..' + token.contract.substring(token.contract.length - 4, token.contract.length)}</a>}
+          {token.blockchain == "Avalanche" && <a className="tokenURL" href={"https://snowtrace.io/token/" + token.contract} target="_blank">{token.contract.substring(0, 4) + '..' + token.contract.substring(token.contract.length - 4, token.contract.length)}</a>}
+          </td>
         <td className="token-blockchain"><div className="blockchain">
           <div className="token-blockchain-images">
           {token.blockchain == "BNB" && <img src="bnb.png"></img>}
@@ -22,7 +30,8 @@ export default function Newlisting({tokens}) {
           </td>
         <td className="token-timestamp">{token.created_at}</td>
       </tr>
-      
+      <hr></hr>
+
       </>
         )
   return (
@@ -46,7 +55,7 @@ export default function Newlisting({tokens}) {
   <th className="token-blockchain">Blockchain</th>
   <th className="token-timestamp">Added</th>
 </tr>
-<div className="table-line"></div>
+<hr></hr>
 
 </thead>
 <tbody>
