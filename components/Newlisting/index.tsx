9fb118c6ea5956627dcb3edb3524a2959d62b025
Lucas <loukaka41@gmail.com>
2022-03-31 17:14:07 +0200

    go

--- components/Newlisting/index.tsx
+++ components/Newlisting/index.tsx
@@ -1,59 +1,57 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-
-export default function Newlisting({tokens}) {
-    const tokenList = tokens.slice(0, 10).map(token => {
-      let date = new Date(token.created_at);
+function Token({id, logo, name, symbol, chat, discord, website, twitter, contract, blockchain, created_at}) {
+  let date = new Date(created_at);
       let seconds = date.getTime();
       let postedDate = Math.round((Date.now() - seconds)/ 1000);
       let format = "";
-      if(postedDate < 60) {
-        format = "seconds";
-      }
-      else if(60 <= postedDate && postedDate < 120)
-      {
-        format = "minute"
-      }
-      else if(120 <= postedDate && postedDate < 3600) {
-        format = "minutes"
-      }
-      else if(3600 <= postedDate && postedDate < 7200) {
-        format = "hour"
-      }
-      else if(7200 <= postedDate && postedDate < 86400) {
-        format ="hours"
-      }
-      else if(86400 <= postedDate && postedDate < 172800) {
-        format = "day"
-      }
-      else if(172800 <= postedDate) {
-        format = "days"
-      }
-       return  <>
-      
-      <tr className="token-container">
-        <td  className="token-id">{tokens.indexOf(token) + 1}</td>
-        <td className="token-name"><img src={token.logo} className="token-logo" alt="token logo" />{token.name}<span className="token-symbol">{token.symbol}</span></td>
+  if(postedDate < 60) {
+    format = "seconds";
+  }
+  else if(60 <= postedDate && postedDate < 120)
+  {
+    format = "minute"
+  }
+  else if(120 <= postedDate && postedDate < 3600) {
+    format = "minutes"
+  }
+  else if(3600 <= postedDate && postedDate < 7200) {
+    format = "hour"
+  }
+  else if(7200 <= postedDate && postedDate < 86400) {
+    format ="hours"
+  }
+  else if(86400 <= postedDate && postedDate < 172800) {
+    format = "day"
+  }
+  else if(172800 <= postedDate) {
+    format = "days"
+  }
+  return (
+      <>
+    <tr className="token-container">
+        <td  className="token-id">{id}</td>
+        <td className="token-name"><img src={logo} className="token-logo" alt="token logo" />{name}<span className="token-symbol">{symbol}</span></td>
         <td className="token-links">
-          {token.chat ? <a href={token.chat} target="_blank"><img className="links-icons" src="telegram.png"></img></a> : ""}
-          {token.discord ? <a href={token.discord} target="_blank"><img className="links-icons" src="discord.png"></img></a> : ""}
-          {token.website ? <a href={token.website} target="_blank"><img className="links-icons" src="website.png"></img></a> : ""}
-          {token.twitter ? <a href={token.twitter} target="_blank"><img className="links-icons" src="twitter.png"></img></a> : ""}</td>
-        <td className="token-contract">
-          {token.blockchain == "BNB" && <a className="tokenURL" href={"https://bscscan.com/token/" + token.contract} target="_blank">{token.contract.substring(0, 4) + '..' + token.contract.substring(token.contract.length - 4, token.contract.length)}</a>}
-          {token.blockchain == "Ethereum" && <a className="tokenURL" href={"https://etherscan.com/token/" + token.contract} target="_blank">{token.contract.substring(0, 4) + '..' + token.contract.substring(token.contract.length - 4, token.contract.length)}</a>}
-          {token.blockchain == "Avalanche" && <a className="tokenURL" href={"https://snowtrace.io/token/" + token.contract} target="_blank">{token.contract.substring(0, 4) + '..' + token.contract.substring(token.contract.length - 4, token.contract.length)}</a>}
+          {chat ? <a href={chat} target="_blank"><img className="links-icons" src="telegram.png"></img></a> : ""}
+          {discord ? <a href={discord} target="_blank"><img className="links-icons" src="discord.png"></img></a> : ""}
+          {website ? <a href={website} target="_blank"><img className="links-icons" src="website.png"></img></a> : ""}
+          {twitter ? <a href={twitter} target="_blank"><img className="links-icons" src="twitter.png"></img></a> : ""}
           </td>
-        <td className="token-blockchain"><div className="blockchain">
-          <div className="token-blockchain-images">
-          {token.blockchain == "BNB" && <img src="bnb.png"></img>}
-          {token.blockchain == "Ethereum" && <img src="eth.png"></img>}
-          {token.blockchain == "Avalanche" && <img src="avax.png"></img>}
-          </div>
-          <span className="token-blockchain-name">{token.blockchain}</span>
+          <td className="token-contract">
+          {blockchain == "BNB" && <a className="tokenURL" href={"https://bscscan.com/token/" + contract} target="_blank">{contract.substring(0, 4) + '..' + contract.substring(contract.length - 4, contract.length)}</a>}
+          {blockchain == "Ethereum" && <a className="tokenURL" href={"https://etherscan.com/token/" + contract} target="_blank">{contract.substring(0, 4) + '..' + contract.substring(contract.length - 4, contract.length)}</a>}
+          {blockchain == "Avalanche" && <a className="tokenURL" href={"https://snowtrace.io/token/" + contract} target="_blank">{contract.substring(0, 4) + '..' + contract.substring(contract.length - 4, contract.length)}</a>}
+          </td>
+          <td className="token-blockchain">
+          <div className="blockchain">
+          {blockchain == "BNB" && <img src="bnb.png"></img>}
+          {blockchain == "Ethereum" && <img src="eth.png"></img>}
+          {blockchain == "Avalanche" && <img src="avax.png"></img>}
+          
+          <span className="token-blockchain-name">{blockchain}</span>
         </div>
-
-          </td>
+        </td>
         <td className="token-timestamp">
           
           {format == "seconds" && <span>{postedDate} seconds ago</span> }
@@ -64,43 +62,9 @@ export default function Newlisting({tokens}) {
           {format == "day" && <span>{Math.floor(postedDate/86400)} day ago</span>}
           {format == "days" && <span>{Math.floor(postedDate/86400)} days ago</span>}
           </td>
-        
       </tr>
-
       </>
-    }
-        )
-      
-    
-  return (
-    <div className="listing">
-    <div className="container">
-    
-        <header>
-            <h1>Recently Added</h1>
-            <span>
-            Find out what are the latest tokens listed on Mobula!
-            </span>
-        </header>
-        <div className="line"></div>
-    <table>
-<thead>
-<tr className="table-head">
-  <th className="token-id">#</th>
-  <th className="token-name">Name</th>
-  <th className="token-links">Links</th>
-  <th className="token-contract">Contract</th>
-  <th className="token-blockchain">Blockchain</th>
-  <th className="token-timestamp">Added</th>
-</tr>
-
-</thead>
-<tbody>
-{tokenList}
-</tbody>
-</table>
-    </div>
-    </div>
   )
 }
 
+export default Token
