9581edfcd0a661c9ba19f995ae0f9fa1d106b374
Lucas <loukaka41@gmail.com>
2022-03-31 15:57:26 +0200

    go

--- components/Newlisting/index.tsx
+++ components/Newlisting/index.tsx
@@ -2,11 +2,37 @@ import React, { useEffect, useState } from 'react'
 
 
 export default function Newlisting({tokens}) {
-    const tokenList = tokens.slice(0, 10).map(token =>
-      <>
+    const tokenList = tokens.slice(0, 10).map(token => {
+      let date = new Date(token.created_at);
+      let seconds = date.getTime();
+      let postedDate = Math.round((Date.now() - seconds)/ 1000);
+      let format = "";
+      if(postedDate < 60) {
+        format = "seconds";
+      }
+      else if(60 <= postedDate && postedDate < 120)
+      {
+        format = "minute"
+      }
+      else if(120 <= postedDate && postedDate < 3600) {
+        format = "minutes"
+      }
+      else if(3600 <= postedDate && postedDate < 7200) {
+        format = "hour"
+      }
+      else if(7200 <= postedDate && postedDate < 86400) {
+        format ="hours"
+      }
+      else if(86400 <= postedDate && postedDate < 172800) {
+        format = "day"
+      }
+      else if(172800 <= postedDate) {
+        format = "days"
+      }
+       return  <>
       
-      <tr>
-        <td className="token-id"></td>
+      <tr className="token-container">
+        <td  className="token-id">{tokens.indexOf(token) + 1}</td>
         <td className="token-name"><img src={token.logo} className="token-logo" alt="token logo" />{token.name}<span className="token-symbol">{token.symbol}</span></td>
         <td className="token-links">
           {token.chat ? <a href={token.chat} target="_blank"><img className="links-icons" src="telegram.png"></img></a> : ""}
@@ -28,15 +54,28 @@ export default function Newlisting({tokens}) {
         </div>
 
           </td>
-        <td className="token-timestamp">{token.created_at}</td>
+        <td className="token-timestamp">
+          
+          {format == "seconds" && <span>{postedDate} seconds ago</span> }
+          {format == "minute" && <span>{Math.floor(postedDate/60)} minute ago</span>}
+          {format == "minutes" && <span>{Math.floor(postedDate/60)} minutes ago</span>}
+          {format == "hour" && <span>{Math.floor(postedDate/3600)} hour ago</span>}
+          {format == "hours" && <span>{Math.floor(postedDate/3600)} hours ago</span>}
+          {format == "day" && <span>{Math.floor(postedDate/86400)} day ago</span>}
+          {format == "days" && <span>{Math.floor(postedDate/86400)} days ago</span>}
+          </td>
+        
       </tr>
-      <hr></hr>
 
       </>
+    }
         )
+      
+    
   return (
     <div className="listing">
     <div className="container">
+    
         <header>
             <h1>Recently Added</h1>
             <span>
@@ -44,10 +83,9 @@ export default function Newlisting({tokens}) {
             </span>
         </header>
         <div className="line"></div>
-    <div className="tokenslisting">
     <table>
 <thead>
-<tr>
+<tr className="table-head">
   <th className="token-id">#</th>
   <th className="token-name">Name</th>
   <th className="token-links">Links</th>
@@ -55,14 +93,12 @@ export default function Newlisting({tokens}) {
   <th className="token-blockchain">Blockchain</th>
   <th className="token-timestamp">Added</th>
 </tr>
-<hr></hr>
 
 </thead>
 <tbody>
 {tokenList}
 </tbody>
 </table>
-    </div>
     </div>
     </div>
   )
