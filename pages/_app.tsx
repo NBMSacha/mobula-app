7991907cf8da88ca4674fc68285ee3b61016a8da
GhostDev3 <103851326+GhostDev3@users.noreply.github.com>
2022-05-06 17:24:54 +0200

    Dashboard page Finish

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react';
-import App from 'next/app'
-import Head from 'next/head'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import { Web3Provider } from '@ethersproject/providers'
-import { Web3ReactProvider } from '@web3-react/core'
-import '../styles.css';
-import '../styles/header.css'
-import '../styles/responsive.css'
-import "../components/Newlisting/Newlisting.css"
+import { useState, useEffect } from "react";
+import App from "next/app";
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Web3Provider } from "@ethersproject/providers";
+import { Web3ReactProvider } from "@web3-react/core";
+import "../styles.scss";
+import "../styles/header.scss";
+import "../styles/responsive.scss";
+import "../components/Newlisting/Newlisting.css";
 import { positions, Provider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const alertOptions = {
   timeout: 5000,
-  position: positions.BOTTOM_CENTER
+  position: positions.BOTTOM_CENTER,
 };
 
-
 function getLibrary(provider: any): Web3Provider {
-  const library = new Web3Provider(provider, "any")
-  library.pollingInterval = 12000
-  return library
+  const library = new Web3Provider(provider, "any");
+  library.pollingInterval = 12000;
+  return library;
 }
 
 export default class Root extends App {
@@ -30,7 +30,6 @@ export default class Root extends App {
 
     return (
       <>
-
         <Head>
           <title>Mobula App</title>
           <link rel="icon" type="image/png" href="/fullicon.png" />
@@ -38,12 +37,14 @@ export default class Root extends App {
 
         <Provider template={AlertTemplate} {...alertOptions}>
           <Web3ReactProvider getLibrary={getLibrary}>
-            <Header />
-            <Component {...pageProps}></Component>
-            <Footer />
+            <ChakraProvider>
+              <Header />
+              <Component {...pageProps}></Component>
+              <Footer />
+            </ChakraProvider>
           </Web3ReactProvider>
         </Provider>
       </>
-    )
+    );
   }
 }
