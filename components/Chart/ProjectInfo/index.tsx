a5fc2070be85400c32cf54b84970576394821d32
Veeno_Dev <77930262+Veenoway@users.noreply.github.com>
2022-05-21 00:29:37 +0200

    Social Link and audit on chart page  DONE

--- components/Chart/ProjectInfo/index.tsx
+++ components/Chart/ProjectInfo/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react'
+import {useRouter} from 'next/router'
 import axios from 'axios'
 import { Chart, ChartType, registerables } from 'chart.js'
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
@@ -33,23 +34,30 @@ const ProjectInfo = ({ token }) => {
             }
         }
     }
-
+    console.log(token)
+    const router = useRouter()
     return (
         <div className={styles["main-container"]}>
             <div className={styles["left"]}>
                 <div className={token.kyc == null && token.audit == null ? styles["left-top-box-center"] : styles["left-top-box"]}>
                     <div className={styles["social-links"]}>
-                        <a href={token.website}><Globe className={styles["icons"]} /></a>
-                        <a href={token.twitter}><Twitter className={styles["icons"]} /></a>
-                        <a href={token.chat}><Send className={styles["icons"]} /></a>
+                        {token.website !== "" && ( 
+                            <a href={token.website} target="_blank"><Globe className={styles["icons"]} /></a>
+                        )}
+                        {token.twitter !== "" && ( 
+                            <a href={token.twitter} target="_blank"><Twitter className={styles["icons"]} /></a>
+                        )}
+                        {token.chat !== "" && ( 
+                            <a href={token.chat} target="_blank"><Send className={styles["icons"]} /></a>
+                        )}
                     </div>
 
                     <div className={styles["audit-links"]}>
-                        {token.kyc !== null && (
-                            <button className={styles["kyc"]}>KYC</button>
+                        {token.kyc !== "" || token.kyc !== null && (
+                            <a href={token.kyc} className={styles["kyc"]}>KYC</a>
                         )}
-                        {token.audit !== null && (
-                            <button className={styles["audit"]}>Audit</button>
+                        {token.audit !== "" || token.audit !== null && (
+                            <a href={token.audit} className={styles["audit"]}>Audit</a>
                         )}
                     </div>
                 </div>
