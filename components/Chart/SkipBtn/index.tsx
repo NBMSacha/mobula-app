48ee06913e9ed8545b59156eaefc4d3fc797c10d
Sacha Marcus <89188530+NBMSacha@users.noreply.github.com>
2022-05-24 16:13:24 +0200

    Fixing Skip Btn

--- components/Chart/SkipBtn/index.tsx
+++ components/Chart/SkipBtn/index.tsx
@@ -19,22 +19,20 @@ import { useRouter } from 'next/router'
 const SkipBtn = ({ beforeToken, afterToken }) => {
     const router = useRouter()
 
-    console.log(afterToken.id)
-
     return (
         <>
             <div className={styles["skip-btn-container"]}>
-                <div className={styles["left-skip-btn"]} onClick={() => document.location.href = getUrlFromName(beforeToken.name)}>
+                <div className={styles["left-skip-btn"]} onClick={() => document.location.href = getUrlFromName(beforeToken ? beforeToken.name : '')}>
                     <button className={styles["blue-skip-btn"]}><ArrowLeft className={styles["arrows"]} /></button>
                     <div className={styles["rank-box-left"]}>
-                        <span className={styles["grey-rank"]}>RANK #{beforeToken.rank}</span>
-                        <span className={styles["white-name"]}>{beforeToken.name}</span>
+                        <span className={styles["grey-rank"]}>RANK #{beforeToken ? beforeToken.rank : ''}</span>
+                        <span className={styles["white-name"]}>{beforeToken ? beforeToken.name : ''}</span>
                     </div>
                 </div>
-                <div className={styles["right-skip-btn"]} onClick={() => document.location.href = getUrlFromName(afterToken.name)}>
+                <div className={styles["right-skip-btn"]} onClick={() => document.location.href = getUrlFromName(afterToken ? afterToken.name : '')}>
                     <div className={styles["rank-box-right"]}>
-                        <span className={styles["grey-rank"]}>RANK #{afterToken.rank}</span>
-                        <span className={styles["white-name"]}>{afterToken.name}</span>
+                        <span className={styles["grey-rank"]}>RANK #{afterToken ? afterToken.rank : ''}</span>
+                        <span className={styles["white-name"]}>{afterToken ? afterToken.name : ''}</span>
                     </div>
                     <button className={styles["blue-skip-btn"]}><ArrowRight size={40} className={styles["arrows"]} /></button>
                 </div>
