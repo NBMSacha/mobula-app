ca29c06649c0a3643681b1564fc12e27d45f573f
Veeno_Dev <77930262+Veenoway@users.noreply.github.com>
2022-05-27 20:40:35 +0200

    TOP 100 DONE

--- components/Main/Block/MainBlock/index.tsx
+++ components/Main/Block/MainBlock/index.tsx
@@ -77,69 +77,70 @@ function MainBlock(props: any) {
           }}
         />
       </div>
-    )
-  } else if (isDisplayed) {
-    return (
-      <div className={styles['container-news']}>
-        <div className={styles['text-news']}>
-          <h2 className={styles['title-news']}>Today's Crypto-assets  by <span>Mobula</span></h2>
-          <p className={styles['subtitle-news']}>
-          The global crypto total value locked is <b>$105M</b> a <a
-              className={styles['subtitle-news-link']}
-              href='https://discord.gg/2a8hqNzkzN'
-            >1.05%</a> decrease over the last day.
+    )}
+  // } else if (isDisplayed) {
+  //   return (
+  //     <div className={styles['container-news']}>
+  //       <div className={styles['text-news']}>
+  //         <h2 className={styles['title-news']}>Today's Crypto-assets  by <span>Mobula</span></h2>
+  //         <p className={styles['subtitle-news']}>
+  //         The global crypto total value locked is <b>$105M</b> a <a
+  //             className={styles['subtitle-news-link']}
+  //             href='https://discord.gg/2a8hqNzkzN'
+  //           >1.05%</a> decrease over the last day.
 
-          </p>
-        </div>
-        <div className={styles['quadBox-container']}>
-          <div className={styles['quad-box-2']}>
-            <a href='https://docs.mobula.finance' className={styles["disabled"]}>
-              <div className={`${styles["texts-box-1"]} ${styles["texts-box"]}`}>
-                <div className={styles['logo-box']}>
-                  <img className={styles.inside} src='fullicon.png' />
-                </div>
-                <p className={styles.text}>
-                  Discover Mobula
-                  <br />
-                  <span className={styles['bold-text']}>Learn and earn $MOBL</span>
-                </p>
-              </div>
-            </a>
-          </div>
+  //         </p>
+  //       </div>
+  //       <div className={styles['quadBox-container']}>
+  //         <div className={styles['quad-box-2']}>
+  //           <a href='https://docs.mobula.finance' className={styles["disabled"]}>
+  //             <div className={`${styles["texts-box-1"]} ${styles["texts-box"]}`}>
+  //               <div className={styles['logo-box']}>
+  //                 <img className={styles.inside} src='fullicon.png' />
+  //               </div>
+  //               <p className={styles.text}>
+  //                 Discover Mobula
+  //                 <br />
+  //                 <span className={styles['bold-text']}>Learn and earn $MOBL</span>
+  //               </p>
+  //             </div>
+  //           </a>
+  //         </div>
 
-          <div className={styles['quad-box-2']}>
-            <a href='https://discord.gg/2a8hqNzkzN'>
-              <div className={`${styles["texts-box-2"]} ${styles["texts-box"]}`}>
-                <div className={styles['logo-box']}>
-                  <img className={styles.inside} src='portfolio.png'></img>
-                </div>
-                <p className={styles.text}>
-                  Portfolio
-                  <img src="fire.png" height="15px" className={styles["marginR"]} />
-                  <br />
-                  <span className={styles['bold-text']}>Vote, earn, interact.</span>
-                </p>
-              </div>
-            </a>
-          </div>
-          <div className={styles['quad-box-2']}>
-            <a href='https://discord.gg/2a8hqNzkzN'>
-              <div className={`${styles["texts-box-2"]} ${styles["texts-box"]}`}>
-                <div className={styles['logo-box']}>
-                  <img className={styles.inside} src='Imagedao.png'></img>
-                </div>
-                <p className={styles.text}>
-                  Join the DAO
-                  <br />
-                  <span className={styles['bold-text']}>Vote, earn, interact.</span>
-                </p>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-    )
-  } else {
+  //         <div className={styles['quad-box-2']}>
+  //           <a href='https://discord.gg/2a8hqNzkzN'>
+  //             <div className={`${styles["texts-box-2"]} ${styles["texts-box"]}`}>
+  //               <div className={styles['logo-box']}>
+  //                 <img className={styles.inside} src='portfolio.png'></img>
+  //               </div>
+  //               <p className={styles.text}>
+  //                 Portfolio
+  //                 <img src="fire.png" height="15px" className={styles["marginR"]} />
+  //                 <br />
+  //                 <span className={styles['bold-text']}>Vote, earn, interact.</span>
+  //               </p>
+  //             </div>
+  //           </a>
+  //         </div>
+  //         <div className={styles['quad-box-2']}>
+  //           <a href='https://discord.gg/2a8hqNzkzN'>
+  //             <div className={`${styles["texts-box-2"]} ${styles["texts-box"]}`}>
+  //               <div className={styles['logo-box']}>
+  //                 <img className={styles.inside} src='Imagedao.png'></img>
+  //               </div>
+  //               <p className={styles.text}>
+  //                 Join the DAO
+  //                 <br />
+  //                 <span className={styles['bold-text']}>Vote, earn, interact.</span>
+  //               </p>
+  //             </div>
+  //           </a>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   )
+  // }
+   else {
     return (
       <div className={styles['container-news']}>
         <div className={styles['text-news']}>
