35c2284ecbaf195ef15fa4a1d6df824d9eabece9
Sacha Marcus <89188530+NBMSacha@users.noreply.github.com>
2022-06-20 15:11:29 +0200

    Fixing listing success, contract logic

--- components/Utils/Contract/index.tsx
+++ components/Utils/Contract/index.tsx
@@ -3,6 +3,7 @@ import { getBlockchainFromContract } from '../../../helpers/blockchain';
 import { Flex, Image, Text, useColorModeValue } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react'
 import { CheckCircle, Copy } from 'react-feather';
+import { supportedRPCs } from '../../../constants';
 export default function Contract({ contract, blockchain }) {
     const [copied, setCopied] = useState(false);
     useEffect(() => {
@@ -19,20 +20,21 @@ export default function Contract({ contract, blockchain }) {
             align="center"
             position="relative"
             justify="left"
-            minWidth={["135px","135px","165px","165px"]}
+            minWidth={["135px", "135px", "165px", "165px"]}
             bg={contracts}
             mt='18px'
-            mx={["auto","auto","auto","0px"]}
+            mx={["auto", "auto", "auto", "0px"]}
             h={["20px", "20px", "45px", "40px"]}
             p="0px 10px"
-            
-            boxShadow={useColorModeValue('0px 1px 6px 1px #d0d6e3', '0px 1px 12px 3px rgba(0,0,0,0.2)')}
-            borderRadius={["6px", "6px" ,'10px','10px']}
 
+            boxShadow={useColorModeValue('0px 1px 6px 1px #d0d6e3', '0px 1px 12px 3px rgba(0,0,0,0.2)')}
+            borderRadius={["6px", "6px", '10px', '10px']}
         >
             {blockchain ? <Image width={'17px'} borderRadius="50%" height={'17px'} src={"/" + blockchain.split(' ')[0].toLowerCase() + '.png'} ml="5px" /> : <></>
             }
-            <Text ml="10px" py={5} fontSize={["10px","10px","15px","15px"]} textAlign="center">{contract.slice(0, 6) + '...' + contract.slice(contract.length - 5, contract.length - 1)}</Text>
+            <Text onClick={() => {
+                window.open(supportedRPCs[supportedRPCs.map(r => r.name).indexOf(blockchain)].explorer + '/token/' + contract)
+            }} ml="10px" py={5} fontSize={["10px", "10px", "15px", "15px"]} textAlign="center">{contract.slice(0, 6) + '...' + contract.slice(contract.length - 5, contract.length - 1)}</Text>
             {
                 copied ? <CheckCircle width='17px' color='#32C784' style={{ "position": "absolute", "right": "10px" }} /> : <Copy width={'15px'} cursor="pointer" style={{ "position": "absolute", "right": "10px" }} onClick={() => {
                     setCopied(true);
